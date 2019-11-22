fd9f54e7c42473ebfd6ebf7a328f6b4e11a0c7e7
Natalie Cygan <nataliemcygan@gmail.com>
2019-11-22 13:46:47 -0800

    added files for building + training a model

---
+++ models/network.py
@@ -0,0 +1,11 @@
+import torch
+import torch.nn as nn
+from torch.nn import functional as F
+
+class Model(nn.Module):
+    def __init__(self, batch_size, output_size):
+        pass
+
+    def forward(self):
+        pass
+
---
+++ train.py
@@ -0,0 +1,12 @@
+import os
+import torch
+import torch.nn.functional as F
+import numpy as np
+from models.network import Model
+
+
+def train_model(model, train_iter, epoch):
+    pass
+
+def eval_model(model, val_iter):
+    pass
