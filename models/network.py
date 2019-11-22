142cf1576822f499366a7a9c8e55d3f3bea5fbee
Natalie Cygan <nataliemcygan@gmail.com>
2019-11-22 14:30:42 -0800

    Created basic two-layer net in models folder and started training harness.

--- models/network.py
+++ models/network.py
@@ -2,10 +2,23 @@ import torch
 import torch.nn as nn
 from torch.nn import functional as F
 
-class Model(nn.Module):
-    def __init__(self, batch_size, output_size):
-        pass
+class FoodDesertClassifer(nn.Module):
+    def __init__(self, input_size=10, hidden_size=10, output_size=2):
+        self.linear1 = nn.Linear(input_size, hidden_size)
+        self.linear2 = nn.Linear(hidden_size, output_size)
 
-    def forward(self):
-        pass
+    """
+    forward pass the classification neural network. Accepts x as parameter,
+    which is the census feature vector for a zipcode.
+    """
+    def forward(self, x):
+        x = x.view(x.size(0), 1) # Flatten tensor
+
+        # First fully connected linear layer.
+        out = self.linear(x)
+        # First layer activation non-linearity function.
+        out = F.relu(out)
+        # Get predictions from final output layer.
+        out = self.linear2(out)
+        return out
 
--- train.py
+++ train.py
@@ -2,11 +2,36 @@ import os
 import torch
 import torch.nn.functional as F
 import numpy as np
-from models.network import Model
+from models.network import FoodDesertClassifier
+import pickle
+import random
+from tqdm import trange
+
+"""
+Program that trains and runs the the food desert classifier network.
+"""
+def main(argv):
+
+    # TODO: read in data
+    data = None
+    model = FoodDesertClassifier()
+
+    data_size = len(data)
+    data = random.shuffle(data)
+    train = data[:(data_size // 10) * 9]
+    test = data[(data_size // 10) * 9:]
+    
 
 
 def train_model(model, train_iter, epoch):
+    total_epoch_loss = 0
+    total_epoch_acc = 0
+    model.cuda()
+
     pass
 
 def eval_model(model, val_iter):
     pass
+
+if __name__ == "__main__":
+   main(sys.argv[1:])
