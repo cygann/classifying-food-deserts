a46ea3033f886c5579d17284ae6df189228c2712
Grace Kim <grace.km001@gmail.com>
2019-11-30 15:31:25 -0800

    NN boilerplate -not complete

--- models/network.py
+++ models/network.py
@@ -3,23 +3,55 @@ import torch.nn as nn
 from torch.nn import functional as F
 
 class FoodDesertClassifier(nn.Module):
-    def __init__(self, input_size=10, hidden_size=10, output_size=2):
+    def __init__(self, input_size=10, hidden_dim_list=[5, 10, 8, 12], output_size=2):
         super(FoodDesertClassifier, self).__init__()
-        self.linear1 = nn.Linear(input_size, hidden_size)
-        self.linear2 = nn.Linear(hidden_size, output_size)
+        self.linear1 = nn.Linear(input_size, hidden_dim_list[0])
+        self.linear2 = nn.Linear(hidden_dim_list[0], hidden_dim_list[1])
+        self.linear3 = nn.Linear(hidden_dim_list[1], hidden_dim_list[2])
+        self.linear4 = nn.Linear(hidden_dim_list[2], hidden_dim_list[3])
+        #self.input_size = input_size
+        self.output = nn.Linear(hidden_dim_list[3], output_size)
+        #self.size_list = hidden_dim_list
+        #self.softmax = nn.Softmax(dim = 1)
+        self.ReLU = nn.ReLU()
+        
 
     """
     forward pass the classification neural network. Accepts x as parameter,
     which is the census feature vector for a zipcode.
     """
     def forward(self, x):
-        x = x.view(x.size(0), 1) # Flatten tensor
-
-        # First fully connected linear layer.
-        out = self.linear(x)
-        # First layer activation non-linearity function.
-        out = F.relu(out)
-        # Get predictions from final output layer.
-        out = self.linear2(out)
-        return out
-
+#        x = x.view(x.size(0), 1) # Flatten tensor
+#
+#        # First fully connected linear layer.
+#        out = self.linear(x)
+#        # First layer activation non-linearity function.
+#        out = F.relu(out)
+#        # Get predictions from final output layer.
+#        out = self.linear2(out)
+#        return out
+        
+        #x = x.view(x.size(0), 1)
+        #---------------------------------------
+        x = self.linear1(x)
+        x = self.ReLU(x)
+        x = self.linear2(x)
+        x = self.ReLU(x)
+        x = self.linear3(x)
+        x = self.ReLU(x)
+        x = self.linear4(x)
+        x = self.ReLU(x)
+        x = self.output(x)
+        return x
+        #x = self.softmax(x)
+        
+#        x = nn.Linear(self.input_size, self.size_list[0])
+#        x = self.ReLU(x)
+#        print("first: ", x)
+#        for i in range(1, len(self.size_list)):
+#            x = nn.Linear(self.size_list[i-1], self.size_list[i])
+#            x = self.ReLU(x)
+#            print("next: ", x)
+#        x = self.output(x)
+#        print("last: ", x)
+#        return x
--- train.py
+++ train.py
@@ -5,6 +5,7 @@ import torch.nn as nn
 from torch.autograd import Variable
 import numpy as np
 from models.single_layer import LogisticRegressionModel
+from models.network import FoodDesertClassifier
 import pickle
 import random
 from tqdm import trange
@@ -36,9 +37,15 @@ def main(argv):
 
     input_dim = len(data[0][0]) # number of features
     output_dim = 2 # two classes: food desert and not food desert
+    hidden_dim_list = [5, 10, 8, 12]
 
+    model_nn = FoodDesertClassifier(input_dim, hidden_dim_list, output_dim)
+    optimize_nn(model_nn, train_data, val_data, test_data)
+    
     model = LogisticRegressionModel(input_dim, output_dim)
     optimize(model, train_data, val_data, test_data) # train on train data
+    
+    
     
     
     #test(model, test_data) # test on test data
@@ -78,7 +85,7 @@ def optimize(model, train_data, val_data, test_data):
                 optimizer.zero_grad()
     
                 # Forward pass to get output/logits
-                pred = model(x) 
+                pred = model(x)[0]
                 #assert not torch.isnan(pred).any(), pred
                 #print('pred is', pred.data)
     
@@ -102,7 +109,7 @@ def optimize(model, train_data, val_data, test_data):
                         if not np.isnan(x).any():
                             total+=1
                             x = Variable(valueToTensor(x))
-                            pred = model(x)
+                            pred = model(x)[0]
                             pred.unsqueeze_(0) # add a dimension before passing to criterion
                             _, predicted = torch.max(pred.data, 0)
                             num_correct = (num_correct+1 if (predicted[0] == y) else num_correct)
@@ -110,7 +117,9 @@ def optimize(model, train_data, val_data, test_data):
                     accuracy = 100.0 * num_correct / total
                     print('Iteration: {}. Loss: {}. Training Accuracy: {}.'.format(iter, loss.item(), accuracy))
                     
-                if iter == 500:
+                if iter == 200:
+                    weights = model(x)[1]
+                    print(weights)
                     eval_model(model, loss, test_data, "Testing")
                     eval_model(model, loss, val_data, "Validation")
 #                    num_correct = 0
@@ -165,13 +174,102 @@ def eval_model(model, loss, data, testType):
         if not np.isnan(x).any():
             total+=1
             x = Variable(torch.tensor(x).float())
-            pred = model(x)
+            pred = model(x)[0]
             pred.unsqueeze_(0) # add a dimension before passing to criterion
             _, predicted = torch.max(pred.data, 0)
             num_correct = (num_correct+1 if (predicted[0] == y) else num_correct)
     accuracy = 100.0 * num_correct / total
     string = 'Loss: {}. ' + testType + ' Accuracy: {}.'
     print(string.format(loss.item(), accuracy))
+    
+
+def optimize_nn(model, train_data, val_data, test_data):
+    """
+    Optimize neural network model on the training set
+    """
+    print('*******Training*******')
+    
+    #criterion = nn.NLLLoss()
+    criterion = nn.CrossEntropyLoss()
+    optimizer = torch.optim.Adam(model.parameters(), lr=0.003)
+    
+    num_epochs = 5
+    iter = 0
+    for epoch in range(num_epochs):
+        running_loss = 0
+        print('EPOCH', epoch)
+        for x,y in train_data:
+            #print('Iteration number: ', iter)
+            if not np.isnan(x).any():
+                
+                #print('y is:', y)
+                # print('Zipcode:', ZIPCODES[i])
+       
+                # Load data as Variable
+                x = Variable(valueToTensor(np.asmatrix(x)))
+                y = Variable(valueToTensor(y))
+                # print('x is:', x)
+                #print('x is:', x)
+    
+                # Clear gradients w.r.t parameters
+                optimizer.zero_grad()
+    
+                # Forward pass to get output/logits
+                pred = model(x)
+                #assert not torch.isnan(pred).any(), pred
+                #print('pred is', pred.data)
+    
+                # Calculate Loss: softmax --> cross entropy loss
+                #import pdb; pdb.set_trace()
+                loss = criterion(pred.float(), y.long().unsqueeze(0))
+                #print('loss is: ', loss)
+    
+                # Backward pass to get gradients w.r.t parameters
+                loss.backward()
+    
+                # Updating parameters
+                optimizer.step()
+                
+                running_loss += loss.item()
+                
+                iter += 1
+                if iter % 100 == 0:
+                    # Calculate accuracy on train_data
+                    num_correct = 0
+                    total = 0
+                    for x, y in train_data:
+                        if not np.isnan(x).any():
+                            total+=1
+                            x = Variable(valueToTensor(x))
+                            pred = model(x)
+                            pred.unsqueeze_(0) # add a dimension before passing to criterion
+                            _, predicted = torch.max(pred.data, 0)
+                            num_correct = (num_correct+1 if (predicted[0] == y) else num_correct)
+                            #print('num_correct:', num_correct, ', total', len(val_data))
+                    accuracy = 100.0 * num_correct / total
+                    print('Iteration: {}. Loss: {}. Training Accuracy: {}.'.format(iter, loss.item(), accuracy))
+                    
+                if iter == 500:
+                    eval_model_nn(model, loss, test_data, "Testing")
+                    eval_model_nn(model, loss, val_data, "Validation")
+                    return
+        #print(f"Training loss: {running_loss / len(train_data)}")
+
+def eval_model_nn(model, loss, data, testType):
+    num_correct = 0
+    total = 0
+    for x, y in data:
+        if not np.isnan(x).any():
+            total+=1
+            x = Variable(torch.tensor(x).float())
+            pred = model(x)[0]
+            pred.unsqueeze_(0) # add a dimension before passing to criterion
+            _, predicted = torch.max(pred.data, 0)
+            num_correct = (num_correct+1 if (predicted == y) else num_correct)
+    accuracy = 100.0 * num_correct / total
+    string = 'Loss: {}. ' + testType + ' Accuracy: {}.'
+    print(string.format(loss.item(), accuracy))
+    
 
 if __name__ == "__main__":
    main(sys.argv[1:])
